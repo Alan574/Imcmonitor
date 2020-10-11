247edf6b422627f7c52d86cd88343735a0be377f
Alan vitoriano <vitorianoalan375@gmail.com>
2020-10-11 19:46:41 +0000

    coloquei get e set

--- Atleta.java
+++ Atleta.java
@@ -1,4 +1,21 @@
-Class Atleta
+Class Atleta {
 
- String nome;
- int idade;
+     private String nome;
+    private int idade;
+
+    public void setNome(String n) {
+      nome = n;
+    }
+
+    public void setIdade(int n) {
+      idade = n;
+    }
+
+    public String getNome() {
+      return nome;
+    }
+
+    public int getIdade() {
+      return idade;  //simulando retorno com erro
+    }
+}
