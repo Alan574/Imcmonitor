301599bfc0a967a8d75aa38c15a74cca7d8bbcc8
Alan vitoriano <vitorianoalan375@gmail.com>
2020-10-17 22:47:59 +0000

    TesteSimples so para testa

---
+++ testeSimples.java
@@ -0,0 +1,12 @@
+class testeSimples{
+
+public void executar() throw Exception{
+  
+  Atleta atleta = new Atleta();
+  atleta.nome = "Alan vitoriano";
+  alteta.idade = 30;
+
+  System.out.println("Nome =" +atleta.nome);
+  System.out.println("Idade = " +atleta.idade); 
+}
+}
